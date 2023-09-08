9c6a5468923f51887df51c93546ebd27bb7c3473
for-1-big-picture <143938278+for-1-big-picture@users.noreply.github.com>
2023-09-08 21:51:04 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: '有趣的事情',
-  HERO_TITLE_2: '正在发生',
+  //HERO_TITLE_2: '正在发生',
   HERO_TITLE_3: 'Something interesting',
   HERO_TITLE_4: 'is unfolding',
  // HERO_TITLE_5: '',
